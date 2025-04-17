5cfc1971664ff618997ee2f7a26c72abf0dbc0cc
Griotold <gotjd9773@naver.com>
2025-04-17 14:33:04 +0900

    초기 세팅 - application.yml

--- src/main/java/com/sparta/redis_caching/domain/ItemController.java
+++ src/main/java/com/sparta/redis_caching/domain/ItemController.java
@@ -1,6 +1,5 @@
 package com.sparta.redis_caching.domain;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
